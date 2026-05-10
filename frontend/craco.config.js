bd5fbf3453c645dc876ddd7a67bf4f64dfb98652
Ethan <ethanyan1013@gmail.com>
2026-05-09 21:43:38 -0400

    fix craco config with path alias

--- frontend/craco.config.js
+++ frontend/craco.config.js
@@ -1,1 +1,15 @@
-const { removePlugin, pluginByName } = require("@craco/craco"); module.exports = { webpack: { configure: (webpackConfig) = removePlugin(webpackConfig, pluginByName("ForkTsCheckerWebpackPlugin")); return webpackConfig; }, }, }; 
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+    configure: (webpackConfig) => {
+      webpackConfig.plugins = webpackConfig.plugins.filter(
+        (plugin) => plugin.constructor.name !== "ForkTsCheckerWebpackPlugin"
+      );
+      return webpackConfig;
+    },
+  },
+};
